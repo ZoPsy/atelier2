6cb817c4373afb198e7432963cdcd7100ec67d6e
ZoPsy <114932875+ZoPsy@users.noreply.github.com>
2023-01-03 14:34:14 +0100

    Update make_html.sh

--- make_html.sh
+++ make_html.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-JARFILE="${1:-biblio-0.1.jar}"
+JARFILE="${biblio-0.1.jar}"
 
-cat data/_liste.txt data/fg67hIOrf.txt | java -cp $JARFILE epsi.biblio > wwwroot/fg67hIOrf.html
-cat data/_liste.txt data/rt25TYuy0.txt | java -cp $JARFILE epsi.biblio > wwwroot/rt25TYuy0.html
+cat data/_liste.txt data/fg67hIOrf.txt | java -cp $JARFILE epsi.biblio.App > wwwroot/fg67hIOrf.html
+cat data/_liste.txt data/rt25TYuy0.txt | java -cp $JARFILE epsi.biblio.App > wwwroot/rt25TYuy0.html
